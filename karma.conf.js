2ff3dd860b7d3c27fbf96742c0a5968186c92845
Federico Giovagnoli <giovagnolif@ryanair.com>
2014-09-09 10:56:54 +0100

    Add Travis config

--- karma.conf.js
+++ karma.conf.js
@@ -2,70 +2,83 @@
 // Generated on Thu Jul 17 2014 15:03:11 GMT+0100 (GMT Daylight Time)
 
 module.exports = function(config) {
-  config.set({
-
-  // base path that will be used to resolve all patterns (eg. files, exclude)
-  basePath: 'public/components',
-
-
-  // frameworks to use
-  // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-  frameworks: ['mocha', 'chai'],
+  var props = {
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: 'public/components',
 
 
-  // list of files / patterns to load in the browser
-  files: [
-    'platform/platform.js',
-    'ns-elements/**/*.test.html',
-    {pattern: "**/*.js", included: false},
-    {pattern: "**/*.css", included: false},
-    {pattern: "**/*.html", included: false}
-  ],
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai'],
 
 
-  // list of files to exclude
-  exclude: [
-
-  ],
-
-
-  // preprocess matching files before serving them to the browser
-  // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-  preprocessors: {
-
-  },
+    // list of files / patterns to load in the browser
+    files: [
+      'platform/platform.js',
+      'ns-elements/**/*.test.html',
+      {pattern: "**/*.js", included: false},
+      {pattern: "**/*.css", included: false},
+      {pattern: "**/*.html", included: false}
+    ],
 
 
-  // test results reporter to use
-  // possible values: 'dots', 'progress'
-  // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-  reporters: ['progress'],
+    // list of files to exclude
+    exclude: [
+
+    ],
 
 
-  // web server port
-  port: 9876,
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+
+    },
 
 
-  // enable / disable colors in the output (reporters and logs)
-  colors: true,
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
 
-  // level of logging
-  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-  logLevel: config.LOG_INFO,
+    // web server port
+    port: 9876,
 
 
-  // enable / disable watching file and executing tests whenever any file changes
-  autoWatch: false,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-  // start these browsers
-  // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-  browsers: ['Chrome'],
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-  // Continuous Integration mode
-  // if true, Karma captures browsers, runs the tests and exits
-  singleRun: true
-  });
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    }
+  };
+
+  if(process.env.TRAVIS){
+    props.browsers = ['Chrome_travis_ci'];
+    props.singleRun = false;
+  }
+
+  config.set(props);
 };
